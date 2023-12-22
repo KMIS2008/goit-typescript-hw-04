32cbf5f4b57d058853986457308b9440a1c8146e
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-12-22 17:47:43 +0200

    1ts

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,13 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, ReactNode } from 'react';
 
+
+// Встановіть правильні типи пропсів для цього компонента. У ньому є дві властивості: children і onContentEndVisible. 
+// children - це будь-який валідний React вузол, а onContentEndVisible - це функція без аргументів, що повертає void.
+
+// Встановіть правильний тип useRef. Посилання endContentRef використовується для div, який міститься в кінці вмісту.
+
+// Встановіть правильний тип для options (клас також може бути типом для options).
+type Props={
+  children: ReactNode;
+  onContentEndVisible:()=>void;
+}
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement|null>(null);
+
+  type Options = {
+    rootMargin: string,
+    threshold: number,
+    root:null
+  }
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options:Options = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
