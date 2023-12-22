5a9f46fc3e1d8a2f6d2832ab3a65afd5d36b16d0
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-12-22 18:25:12 +0200

    2ts

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,23 @@
 import React, {useReducer} from "react";
 
+// RequestStep: Це рядковий літерал.
+
+// State: Цей тип являє собою об'єкт з двома властивостями isRequestInProgress і RequestStep
+// Action: Це тип, що представляє можливі дії, які можуть бути відправлені до редюсера.
+// Дивіться код і опишіть для нього правильні типи.
+
+type State={
+  isRequestInProgress:boolean,
+  requestStep:string
+}
+
+type Action = 
+ |{type: 'START_REQUEST'}
+ |{type: 'PENDING_REQUEST'}
+ |{type:'FINISH_REQUEST'}
+ |{type:'RESET_REQUEST'}
+
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
